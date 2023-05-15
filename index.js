eaa33a5000b19327316be7c8776c2b3ce84e5c1a
nlnhat28 <nl.nhat28@gmail.com>
2023-05-15 15:37:25 +0700

    Fix: Page content

--- index.js
+++ index.js
@@ -18,13 +18,20 @@ async function typeWriter() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+function changeIconJs1() {
+    var img = document.getElementById('icon-js');
+    img.src = 'images/icon-js-1.png';
+}
+function changeIconJs2() {
+    var img = document.getElementById('icon-js');
+    img.src = 'images/icon-js-2.png';
+}
 function expandTitleJavascript() {
     var bigTitleContainer = document.getElementById("big-title-container");
     if (bigTitleContainer.lastChild.id != "what-is-js") {
         var linknode = document.createElement('a');
         linknode.id = "what-is-js"
-        linknode.href = 'https://xuanthulab.net/javascript/';
+        linknode.href = 'https://en.wikipedia.org/wiki/JavaScript';
         linknode.target = "blank";
         linknode.innerText = "What's Javascript?";
         linknode.style.display = "flex";
@@ -40,4 +47,10 @@ function expandTitleJavascript() {
     else {
         bigTitleContainer.removeChild(bigTitleContainer.lastChild);
     }
+}
+function openFeedbackForm(){
+    document.getElementById('dialog-feedback').showModal();
+}
+function closeFeedbackForm(){
+    document.getElementById('dialog-feedback').close();
 }
